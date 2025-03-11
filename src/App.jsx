b8d25712932ef5ef812c61a3674e96ad46f1eea9
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-03-11 04:31:02 -0300

    Implemented rain, thunder and snowfall effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import './App.css'
+import RainEffect from './components/Rain/RainEffect';
 import WeatherContainer from './components/WeatherContainer/WeatherContainer'
+import Snowflakes from './components/Snowflakes/Snowflakes';
+import Thunder from './components/Thunder/Thunder';
 
 function App() {
+  const [weather, setWeather] = useState(0);
 
   // Parallax effect
   document.addEventListener('mousemove', function (e) {
@@ -11,13 +16,25 @@ function App() {
     const y = e.clientY / window.innerHeight;
 
     body.style.backgroundPosition = `${x * 33}% ${y * 66}%`;
-    overlay.style.backgroundPosition = `${x * 33}% ${y * 66}%`;
+    if (overlay) overlay.style.backgroundPosition = `${x * 33}% ${y * 66}%`;
   });
+
+  const currentWeather = (id) => {
+    setWeather(id);
+  }
 
   return (
     <main>
+      <WeatherContainer currentWeather={currentWeather} />
       <div id="overlay" style={{ opacity: 0 }}></div>
-      <WeatherContainer />
+      {weather >= 200 && weather <= 531 ?
+        <>
+          <RainEffect />
+          {weather >= 200 && weather <= 232 && <Thunder />}
+        </>
+        : weather >= 600 && weather <= 622 ?
+          <Snowflakes />
+          : ""}
     </main>
   )
 }
