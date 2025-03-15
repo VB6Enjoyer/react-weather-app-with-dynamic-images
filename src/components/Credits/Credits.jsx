690f092791b237a4b6aa202a66ea58ad9e339ae2
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-03-15 02:01:45 -0300

    Implemented Credits

---
+++ src/components/Credits/Credits.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import "./Credits.scss"
+
+export default function Credits() {
+    return (
+        <div id="credits-container">
+            <a href="https://github.com/VB6Enjoyer" id="credits">Made by <span id="author">VB6Enjoyer</span></a>
+        </div>
+    )
+}
